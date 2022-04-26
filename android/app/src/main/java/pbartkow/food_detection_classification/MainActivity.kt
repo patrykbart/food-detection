328b27e423352f89df8f6dfab16f3e30495c0a89
Patryk Bartkowiak <patryk@pieces.app>
2022-04-26 22:48:37 +0200

    getting predictions from model

--- android/app/src/main/java/pbartkow/food_detection_classification/MainActivity.kt
+++ android/app/src/main/java/pbartkow/food_detection_classification/MainActivity.kt
@@ -4,16 +4,14 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import org.tensorflow.lite.DataType
-import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
-import org.tensorflow.lite.support.image.ops.ResizeOp
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import pbartkow.food_detection_classification.ml.Model
-import java.nio.ByteBuffer
+import java.io.File
+import kotlin.math.exp
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,10 +34,10 @@ class MainActivity : AppCompatActivity() {
 
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             // Load image
-            var imageBitmap = data!!.extras!!.get("data") as Bitmap
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
 
             // Rescale
-            var rescaledImageBitmap = Bitmap.createScaledBitmap(
+            val rescaledImageBitmap = Bitmap.createScaledBitmap(
                 imageBitmap,
                 224,
                 224,
@@ -58,6 +56,31 @@ class MainActivity : AppCompatActivity() {
             // Run model inference and gets result.
             val outputs = model.process(inputFeature)
             val outputFeature = outputs.outputFeature0AsTensorBuffer
+            val outputFloats= outputFeature.floatArray
+
+            val classes = assets.open("classes.txt").bufferedReader().use{it.readLines()}
+
+            for(x in 1 until outputFeature.shape[1]) {
+                for(y in 1 until outputFeature.shape[2]) {
+
+                    var maxIndex = -1
+                    var maxValue = -1.0f
+                    var classSum = 0.0f
+
+                    for(classNumber in 5 until outputFeature.shape[4]) {
+                        val classValue = exp(outputFloats[x * y * classNumber])
+
+                        if(classValue > maxValue) {
+                            maxIndex = classNumber - 5
+                            maxValue = classValue
+                        }
+
+                        classSum += classValue
+                    }
+
+                    println("PREDICTION: " + classes[maxIndex] + " PROBABILITY: " + maxValue / classSum)
+                }
+            }
 
             // Release model resources if no longer used.
             model.close()
